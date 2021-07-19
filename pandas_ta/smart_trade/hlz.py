4be2095178683dd155d9e5e2e139727c74db4bbc
Abhishek Jain <patni.abhishek.jain@gmail.com>
2021-07-19 18:31:09 +0530

    Added Separate Upper,Lower boundaries

--- pandas_ta/smart_trade/hlz.py
+++ pandas_ta/smart_trade/hlz.py
@@ -4,7 +4,7 @@ from pandas import DataFrame
 from pandas_ta.utils import get_offset, verify_series
 
 
-def hlz(close, size, offset=None, **kwargs):
+def hlz(close, u_bound, l_bound, offset=None, **kwargs):
     """Indicator: HILO_ZONE (HILO_ZONE)"""
     # Validate Arguments
     close = verify_series(close)
@@ -16,19 +16,19 @@ def hlz(close, size, offset=None, **kwargs):
     df.category = "smart-trade"
 
     prev_date = close.first_valid_index().date()
-    upper, lower = close.iloc[0] + size, close.iloc[0] - size
+    upper, lower = close.iloc[0] + u_bound, close.iloc[0] - l_bound
     prev_close = close.iloc[0]
 
     for index, row in df.iterrows():
         if index.date() != prev_date:
-            upper, lower = prev_close + size, prev_close - size
+            upper, lower = prev_close + u_bound, prev_close - l_bound
         prev_close = row["close"]
         prev_date = index.date()
         df.loc[index, "HLZ_HIGH"] = upper
         df.loc[index, "HLZ_LOW"] = lower
         if not lower < row["close"] < upper:
             df.loc[index, "HLZ_ZONE"] = df.loc[index, "HLZ_BREAK"] = 1 if row["close"] >= upper else -1
-            upper, lower = row["close"] + size, row["close"] - size
+            upper, lower = row["close"] + u_bound, row["close"] - l_bound
         else:
             df.loc[index, "HLZ_BREAK"] = 0
 
@@ -65,12 +65,13 @@ Sources:
     https://smart-trade.reluminos.com
 
 Calculation:
-    zone_high[t] = close[t-1] + size if not zone_high[t-1] > close[t-1] > zone_low[t-1] else zone_high[t-1]          
-    zone_low[t] = close[t-1] - size if not zone_high[t-1] > close[t-1] > zone_low[t-1] else zone_low[t-1]
+    zone_high[t] = close[t-1] + u_bound if not zone_high[t-1] > close[t-1] > zone_low[t-1] else zone_high[t-1]
+    zone_low[t] = close[t-1] - l_bound if not zone_high[t-1] > close[t-1] > zone_low[t-1] else zone_low[t-1]
 
 Args:
     close (pd.Series): Series of 'close's
-    size (float): Size of zone to be created
+    u_bound (float): Upper Bound of zone to be created
+    l_bound (float): Lower Bound of zone to be created
     offset (int): How many periods to offset the result. Default: 0
 
 Kwargs:
